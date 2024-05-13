897fc8f1813eb9cdf17d6267338bdb36fce892a8
senira277 <157682552+senira277@users.noreply.github.com>
2024-05-13 16:06:21 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-//  Responsive Navbar
-function toggleMenu(){
-    var menuToggle = document.querySelector('.toggle');
-    var menu = document.querySelector('.menu');
-    menuToggle.classList.toggle('active')
-    menu.classList.toggle('active')
-}
